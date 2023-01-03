e48548d562df08eb49d8cf04fc8c4bbc4c8ed224
PvrkFvmily <wlsgnl9973@gmail.com>
2023-01-03 15:45:36 -0800

    added detail route to show details of a villagers

--- controllers/villagers.js
+++ controllers/villagers.js
@@ -21,12 +21,16 @@ router.get('/', async (req, res) => {
 // GET /species
 router.get('/species', async (req, res) => {
     try {
-        res.send('list all the villagers by species here with ejs')
+        let acnhUrl = 'https://acnhapi.com/v1a/villagers'
+        const response = await axios.get(acnhUrl)
+        // console.log(response.data[0].personality)
+        res.render('villagers/species.ejs', {
+            villager: response.data,
+        })
     } catch (error) {
         console.log('cannot get info', error)
     }
 })
-
 // GET /gender
 router.get('/gender', async (req, res) => {
     try {
@@ -45,14 +49,18 @@ router.get('/hobby', async (req, res) => {
     }
 })
 
-// // GET /:id
-// router.get('/:id', async (req, res) => {
-//     try {
-//         res.send('list all the villagers here with ejs', req.params.id)
-//     } catch (error) {
-//         console.log('cannot get info', error)
-//     }
-// })
+// GET /:id
+router.get('/:id', async (req, res) => {
+    try {
+        let acnhUrl = `https://acnhapi.com/v1a/villagers/${req.params.id}`
+        const response = await axios.get(acnhUrl)
+        res.render('villagers/details.ejs', {
+            villager: response.data
+        })
+    } catch (error) {
+        console.log('cannot get info', error)
+    }
+})
 
 // router export
 
